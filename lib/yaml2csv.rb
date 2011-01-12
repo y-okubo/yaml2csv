9e9b2eeb508b819d68cb96dc0de430e04dfd9a82
Ben Hutton <benhutton@gmail.com>
2011-01-12 13:31:40 -0500

    Fix the code to make the specs pass

--- lib/yaml2csv.rb
+++ lib/yaml2csv.rb
@@ -11,24 +11,24 @@ module Yaml2csv
   # Convert a string containing YAML data to a CSV string
   def self.yaml2csv(yamldata, options = {})
     hash = YAML::load(yamldata)
-    output = ""
-    csv_writer = CSV::BasicWriter.new(output, options[:field_separator])
-    hash.to_enum(:walk).each do |path, key, value|
-      strvalue = value.is_a?(String) ? value : value.inspect
-      csv_writer << [path.join("/"), key, strvalue]
+
+    CSV.generate do |csv_writer|
+      hash.to_enum(:walk).each do |path, key, value|
+        strvalue = value.is_a?(String) ? value : value.inspect
+        csv_writer << [path.join("/"), key, strvalue]
+      end
     end
-    output
   end
   
   # Convert a string containing CSV values to a YAML string
   def self.csv2yaml(csvdata, options = {})
-    rows = CSV::parse(csvdata, options[:field_separator])
-    walk_array = rows.map do |strpath, key, value| 
-      [strpath.split("/").map(&:to_s), key.to_s, value.to_s]
+    walk_array = []
+
+    CSV.parse(csvdata) do |row|
+      walk_array << [row[0].split("/").map(&:to_s), row[1].to_s, row[2].to_s]
     end
+
     hash = Hash.unwalk_from_array(walk_array)
-    $KCODE = "UTF8"
-    # Remove any trailing spaces inserted by String#to_yaml
     hash.ya2yaml.gsub(/\s+$/, '') + "\n"
   end  
 end
